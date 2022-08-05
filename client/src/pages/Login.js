07498f9d07173cf5bf0cd110055a1a0e9956625a
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-08-05 11:56:04 +0530

    Login Page css Done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import "./Login.css";
 import axios from 'axios';
@@ -7,72 +7,81 @@ import axios from 'axios';
 
 
 const Login = () => {
-    
-    const[email, setEmail] = useState("");
-    const[password,setPassword] =useState("");
-    // const[first_Name, setfirst_name]= useState("");
-    
-const loginSubmit = async(e) =>{
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const[first_Name, setfirst_name]= useState("");
+
+  const loginSubmit = async (e) => {
     e.preventDefault();
-     
+
     axios
-    .get(`http://localhost:5002/api/login/${email}/${password}`)
-    .then(res=>{
-    console.log(res.data[0].email);
-    console.log(res.data[0].firstname);
-    localStorage.setItem("userInfo", JSON.stringify(res.data[0]));
-      window.location='/Category';
-    
-    }).catch(err=>{
+      .get(`http://localhost:5002/api/login/${email}/${password}`)
+      .then(res => {
+        console.log(res.data[0].email);
+        console.log(res.data[0].firstname);
+        localStorage.setItem("userInfo", JSON.stringify(res.data[0]));
+        window.location = '/Category';
+
+      }).catch(err => {
         console.log(err);
-    })
-   
-}
+      })
+
+  }
 
   return (
-     <div style={{marginTop: "100px"}}>
-      <label className='labelContainer'> Login </label>
-    <form style={{
-            margin:"auto",
-            padding:"15px",
-            maxWidth:"400px",
-            alignContent:"center"
+    <div style={{ marginTop: "100px" }}>
+      <label className='loginlabelContainer'> Log In to Your Account </label>
+      <form style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "400px",
+        alignContent: "center"
 
-    }} 
-    onSubmit={loginSubmit}>
-   
-   
-    <label className='emailContainer'>Email-Id:</label>
-    <br/>
+      }}
+        onSubmit={loginSubmit}>
+
+
+
+        <br />
         <input
-        className='inputbox-Style'
-        type="email"
-        id="email"
-        name="email"
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}
-        placeholder="Email Id"
-       />
-       <br/>
-       
-       
-        <label className='passwordContainer'>Password:</label>
-        
-        <input
-        className='inputbox-Style'
-        type="password"
-        id="password"
-        name="password"
-        value={password}
-        onChange={(e)=>setPassword(e.target.value)}
-        placeholder="Password"
-          />
+          className='inputboxLoginstyle'
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
         <br/>
-        <input type="submit" value="Login"/>
-        <a href="/">
-            <input type="button" value="Go Back"/>
-        </a>
-    </form>
+        <input
+          className='inputboxLoginstyle'
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <br />
+        <br/>
+        <div className='submitbuttonstyle' >
+        <input type="submit" value="Log In" />
+        </div>
+        <br/>
+        <br/>
+        <div>
+          <label className='secondContainer'>Need an account? </label>
+          
+          <a href='/'>
+            <strong>
+              <label className='signupTextstyle'> SignUp</label>
+            </strong>
+          </a>
+        </div>
+
+      </form>
 
 
     </div>
